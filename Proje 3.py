d2059da9410682f13006daf8511e02cf648e9e68
Mikail Tipi <51881406+mikailtipi@users.noreply.github.com>
2022-01-24 18:18:52 +0300

    Add files via upload

---
+++ Proje 3.py
@@ -0,0 +1,20 @@
+Root = 7 
+
+                7
+              /   \
+            5       8
+          /  \       \
+         1    6       9
+        /  \
+       0    3 
+          /  \
+         2    4 
+
+Root'un sağında 8 vardır.
+Root'un solunda 5 vardır.
+5 in solunda 1 vardır. 
+5 in sağında 6 vardır.
+1 in solunda 0 vardır. 
+1 in sağında 3 vardır. 
+3 ün solunda 2 vardır. 
+3 ün sağında 4 vardır.    
